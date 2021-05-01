c030848947973420d4e3f3c49c9714363b09b1ac
ihatelethargy <naturalflavor1007@naver.com>
2021-05-01 17:08:37 +0900

    recapModel + gitIgonre

---
+++ dbrecap/recap/blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+## model 설계
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+
+
+    ## 여기에 str 넣어준다.
+    def __str__(self):
+        return self.title  
