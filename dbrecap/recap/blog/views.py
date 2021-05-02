0ad70020ae3ff3cd6209e976697615966159ab9e
ihatelethargy <naturalflavor1007@naver.com>
2021-05-03 01:43:04 +0900

    recapCreate

--- dbrecap/recap/blog/views.py
+++ dbrecap/recap/blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 # Blog model 임폴트 과정이 꼭 필요함
 from .models import Blog  
 # Create your views here.
+from django.utils import timezone
 
 def home(request):
     ## models 연결하고 저장
@@ -11,3 +12,15 @@ def home(request):
 def detail(request,id):
     blog = get_object_or_404(Blog,pk=id)
     return render(request, 'detail.html', {'blog':blog})    
+
+def new(request):
+    return render(request,'new.html')
+
+def create(request):
+    new_blog = Blog()
+    new_blog.title = request.POST['title']
+    new_blog.writer = request.POST['writer']
+    new_blog.body = request.POST['body']
+    new_blog.pub_date = timezone.now()
+    new_blog.save()
+    return redirect('detail', new_blog.id)
